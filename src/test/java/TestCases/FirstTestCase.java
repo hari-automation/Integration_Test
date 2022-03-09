3afe7472a0a349577e322d95d8efcfdd926b5e97
hari-automation <101255137+hari-automation@users.noreply.github.com>
2022-03-09 16:34:24 +0530

    Add files via upload

---
+++ src/test/java/TestCases/FirstTestCase.java
@@ -0,0 +1,44 @@
+package TestCases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTestCase {
+		WebDriver driver;
+	@BeforeTest()
+	public void init() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+		driver.get("https://suppliers-master-minus-1-qe1.coupadev.com");
+	}
+	
+	@Test
+	public void validatetitle() {
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Coupa Home");
+	}
+	
+	@Test
+	public void validateImage() {
+		boolean  imageDisplayed = driver.findElement(By.className("logo")).isDisplayed();
+		Assert.assertEquals(imageDisplayed, true);
+		
+	}
+	
+	@AfterTest()
+	public void tearDown(){
+		driver.quit();
+	}
+}
